d689fcacfc38d219f1d95adad7cb120450dd2ce9
HuAlvin <146694764+HuAlvin@users.noreply.github.com>
2023-10-16 13:00:03 +0700

    update views

--- laravel10/routes/web.php
+++ laravel10/routes/web.php
@@ -17,42 +17,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Buat Route ke halaman profil
-Route::get("/profile", function()
- {
-    return view ('profile');
- });
+Route::get('/dosen', function() {
+    return view ('dosen');
+});
 
- //Route dengan parameter (wajib)
- Route::get("/mahasiswa/{nama?}", function($nama = "Alvin")
- {
-    echo "<h1>Halo Nama Saya $nama</h1>";
- });
+Route::get('/dosen/index', function() {
+    return view ('dosen.index');
+});
 
-  Route::get("/mahasiswa/{nama?}/{pekerjaan?}", function($nama = "Alvin", $pekerjaan = "Mahasiswa")
- {
-    echo "<h1>Halo Nama Saya $nama. Saya adalah $pekerjaan</h1>";
- });
-
- //Redirect
-   Route::get("/hubungi", function()
- {
-    echo "<h1>Hubungi Kami</h1>";
- })->name("call");
-
- Route::redirect("/contact", "/hubungi");
-
-  Route::get("/halo", function()
- {
-    echo "<a href ='". route ("call") . ">". route ("call"). "</a>";
- });
-
- Route::prefix("/dosen")->group(function(){
-    Route::get("/jadwal", function(){
-        echo "<h1>Jadwal Dosen</h1>";
-    });
-    Route::get("/materi", function(){
-        echo"<h1>Materi Perkuliahan</h1>";
-    })
-    //dan lain2
- });
+Route::get('/fakultas', function () {
+    // return view('fakultas.index', ["ilkom" => "Fakultas Ilmu Komputer dan Rekayasa"]);
+    // return view ('fakultas.index', ["fakultas" => ["Fakultas Ilmu Komputer dan Rekayasa", "Fakultas Ilmu Ekonomi"]]);
+    // return view('fakultas.index') ->with("fakultas", ["Fakultas Ilmu Komputer dan Rekayasa", "Fakultas Ilmu Ekonomi"]);
+    $kampus = "Universitas Multi Data Palembang";
+    // $fakultas = [];
+    $fakultas = ["Fakultas Ilmu Komputer dan Rekayasa", "Fakultas Ilmu Ekonomi"];
+    return view('fakultas.index', compact('fakultas', 'kampus'));
+});
